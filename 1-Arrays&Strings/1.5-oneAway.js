b584693d8919396a78977aa7ebdc198df869014c
Laith Harb <harblaith77@gmail.com>
2020-05-16 09:10:28 -0400

    first commit

---
+++ 1-Arrays&Strings/1.5-oneAway.js
@@ -0,0 +1,14 @@
+const oneAway = (string1, string2) => {
+    const lengthStr1 = string1.length;
+    const lengthStr2 = string2.length;
+
+    // IF THERE IS A MISMATCH IN LENGTH, WE CAN OMIT REPLACEMENT
+    // IF THE LENGTH DISCREPANCY IS MORE THAN 1, THEN WE RETURN FALSE 
+    if(lengthStr1 )
+
+
+}
+
+
+
+// pales --- pale
